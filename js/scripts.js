e9db2a79128879034efd9dff0ffaaf0e4ab10794
Christopher Cahill and Tanvi Garg <tanvi@>
2018-10-30 16:12:14 -0700

    kyle helping big time

--- js/scripts.js
+++ js/scripts.js
@@ -26,12 +26,15 @@ Tickets.prototype.findTicket = function(id) {
 }
 
 
+
+
 // Business Logic for Contacts ---------
-function Ticket(typeMovie, timeOfDay, guestAge, price) {
+function Ticket(typeMovie, timeOfDay, guestAge) {
   this.typeMovie = typeMovie,
   this.timeOfDay = timeOfDay,
   this.guestAge = guestAge,
-  this.price = price
+  this.price = 0
+
 }
 
 // function TypeOfMovie(nu, rerun) {
@@ -50,38 +53,52 @@ function Ticket(typeMovie, timeOfDay, guestAge, price) {
 //   this.senior = senior
 // }
 
-function TypeOfMovie(value) {
-  if (value === "1") {
-    this.value = "new Movie"
+Ticket.prototype.TypeOfMovie = function() {
+  if (this.typeMovie === "1") {
+    //this.value = "new Movie"
+    this.price += 7
+    console.log("New Movie: " + this.price);
   }
-  else if (value === "2") {
-    this.value = "rerun"
+  else if (this.typeMovie === "2") {
+    //this.value = "rerun"
+    this.price += 5
+    console.log("Rerun: " + this.price);
   }
 }
 
 function TimeOfDay(time) {
   if (time >= 1000 && time < 1800) {
     this.time = "matinee"
+    this.price = 5
   }
   else if (time >= 1800 && time <=2400) {
     this.time = "primetime"
+    this.price = 7
   }
 }
 
 function GuestAge(age) {
   if (age > 1 && age < 15) {
     this.age = "child"
+    this.price = 0
   }
   else if (age >= 15 && age < 65) {
     this.age = "regular"
+    this.price = 2
   }
   else if (age >= 65) {
-    this.age = "senior"}
+    this.age = "senior"
+    this.price = 1
+  }
 }
 
-Ticket.typeMovie = TypeOfMovie();
+//Ticket.typeMovie = TypeOfMovie();
 Ticket.timeOfDay = TimeOfDay();
 Ticket.guestAge = GuestAge();
+
+// Ticket.typeMovie.price + Ticket.timeOfDay.price + Ticket.guestAge.price;
+
+
 
 ///UI Logic ///
 var guestTickets = new Tickets();
@@ -93,21 +110,11 @@ var guestTickets = new Tickets();
 //   $(".time-day").html(ticket.timeOfDay);
 //   $(".guest-age").html(ticket.guestAge);
 // }
-
-function displayTickets(ticketsToDisplay) {
-  var ticketsList = $("ul#outputTix");
-  var htmlForTicketInfo = "";
-  ticketsToDisplay.tickets.forEach(function(ticket) {
-    htmlForTicketInfo += "<li id=" + ticket.id + ">" + ticket.typeMovie.value + " " + ticket.timeOfDay.time + " " + ticket.guestAge.age + "</li>";
-  });
-  ticketsList.html(htmlForTicketInfo);
-};
-
 function displayTickets2(ticketsToDisplay) {
   var ticketsList = $("div#output2");
   var htmlForTicketInfo = "";
   ticketsToDisplay.tickets.forEach(function(ticket) {
-    htmlForTicketInfo += "<div class='p-2 bd-highlight' id=" + ticket.id + ">" + ticket.typeMovie.value + " " + ticket.timeOfDay.time + " " + ticket.guestAge.age + "</div>";
+    htmlForTicketInfo += "<div class='p-2 bd-highlight' id=" + ticket.id + ">" + ticket.typeMovie.value + " " + ticket.typeMovie.price + " " + ticket.timeOfDay.time + " " + ticket.guestAge.age + "</div>";
   });
   ticketsList.html(htmlForTicketInfo);
 };
@@ -122,14 +129,17 @@ $(document).ready(function() {
     var ticketTime = $("input#time-of-day").val();
     var ageOfGuest = $("input#age-of-guest").val();
 
-    var movieKind = new TypeOfMovie(moviePick);
+    //var movieKind = new TypeOfMovie(moviePick);
     var showTime = new TimeOfDay(ticketTime);
     var howOld = new GuestAge(ageOfGuest);
 
-    var newTicket = new Ticket(movieKind, showTime, howOld);
+
+    var newTicket = new Ticket(moviePick, showTime, howOld);
+    newTicket.TypeOfMovie();
+    console.log("User sees: " + newTicket.price);
 
     guestTickets.addTicket(newTicket);
-    displayTickets(guestTickets);
+
     displayTickets2(guestTickets);
   })
 })
